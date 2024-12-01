54240688b604edfac01f4b69089a22d5b5409d2e
Hacoree <feryputrafer@gmail.com>
2024-12-01 20:27:24 +0700

    update src/docs/swagger.js

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -63,4 +63,7 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options);
 
-module.exports = swaggerSpec;
+module.exports = swaggerSpec
+
+"openapi":"3.0.0","info":{"title":"API Documentation","version":"1.0.0","description":"Dokumentasi API dengan Swagger"},"servers":[{"url":"https://unand.vercel.app"}],"components":{"schemas":{"Menfess":{"type":"object","properties":{"id":{"type":"integer","description":"ID menfess yang unik"},"sender":{"type":"string","description":"Pengirim menfess"},"message":{"type":"string","description":"Pesan dari menfess"},"song":{"type":"string","description":"Lagu terkait dengan menfess (opsional)"},"recipient":{"type":"string","description":"Penerima menfess"},"createdAt":{"type":"string","format":"date-time","description":"Tanggal dan waktu saat menfess dibuat"},"updatedAt":{"type":"string","format":"date-time","description":"Tanggal dan waktu saat menfess terakhir diperbarui"}}}}},"paths":{},"tags":[]}
+
