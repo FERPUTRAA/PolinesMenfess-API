82eadacb424d08a2805b84018d363caf67fa624b
Hacoree <feryputrafer@gmail.com>
2024-12-02 20:28:26 +0700

    update src/database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-const supabasekey = process.env.SUPABASE_SERVICE_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey,supabaseserviceKey);
+const supabasekey = process.env.SUPABASE_SERVICE_ROLE;
+const supabase = createClient(supabaseUrl, supabaseAnonKey,supabaseservicerole);
 
 const checkConnection = async () => {
     try {
